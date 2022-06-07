f91e239c281907d7cbb2726c8688fe26cc66197c
vlados <vlados@users.noreply.github.com>
2022-06-07 13:13:16 +0000

    Fix styling

--- tests/Models/TestModel.php
+++ tests/Models/TestModel.php
@@ -23,7 +23,7 @@ class TestModel extends Model
     {
         return 'test-' . Str::slug($this->getAttribute('name'));
     }
-    
+
     public function getUrlHandler(): array
     {
         return [

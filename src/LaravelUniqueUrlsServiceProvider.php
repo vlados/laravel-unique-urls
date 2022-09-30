99574a3a0e5a39240181037e2de62fbc9b133707
Vladislav Stoitsov <dev@vladko.dev>
2022-09-30 23:44:17 +0300

    Add command for rebuilding urls of models

--- src/LaravelUniqueUrlsServiceProvider.php
+++ src/LaravelUniqueUrlsServiceProvider.php
@@ -4,6 +4,7 @@ namespace Vlados\LaravelUniqueUrls;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Vlados\LaravelUniqueUrls\Commands\RebuildUrlsCommand;
 
 class LaravelUniqueUrlsServiceProvider extends PackageServiceProvider
 {
@@ -12,6 +13,7 @@ class LaravelUniqueUrlsServiceProvider extends PackageServiceProvider
         $package
             ->name('laravel-unique-urls')
             ->hasConfigFile()
+            ->hasCommand(RebuildUrlsCommand::class)
             ->hasMigration('create_unique_urls_table');
     }
 }
